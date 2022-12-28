03d5a7887fc988a8b81f55c7ede052940537d47d
Navpreet Singh <mandair96@gmail.com>
2022-12-28 15:43:31 +1300

    Adeded delete methods

--- src/app.js
+++ src/app.js
@@ -41,22 +41,31 @@ app.get('/tasks', (req, res) => {
 app.get('/users/:id', (req, res) => {
     //console.log(req.params)
     User.findById(req.params.id).then((result) => {
-        res.send(result);
+        if(!result) {
+            res.status(404).send({message: 'no user found!'})
+        }
+        else {
+            res.send(result);
+        }
+        
     }).catch((error)=>{
-        res.status(404).send({message: 'no user found!'});
+        res.status(500).send({message: error.message});
     });
 });
 
 app.get('/tasks/:id', (req, res) => {
     //console.log(req.params)
     Task.findById(req.params.id).then((result) => {
-        res.send(result);
+        if(!result) {
+            res.status(404).send({message: 'no task found!'})
+        }
+        else {
+            res.send(result);
+        }
     }).catch((error)=>{
-        res.status(404).send({message: 'no user found!'});
+        res.status(500).send({message: error.message});
     });
 });
-
-
 
 app.post('/users', (req, res) => {
     //console.log(req.body);
@@ -76,6 +85,51 @@ app.post('/tasks', (req, res) => {
     }).catch((error)=>{
         res.status(400).send({message: error.message});
     });
+});
+
+app.delete('/tasks/:id', (req, res) => {
+    //console.log(req.params)
+    Task.findById(req.params.id).then((result) => {
+        if (!result) {
+            return null
+        }
+        else {
+            return Task.findByIdAndDelete(req.params.id)
+        }
+    }).then((result) =>{
+        if (!result) {
+            res.status(404).send({ message: 'no task found!' })
+        }
+        else {
+            res.send({ id: result.id })
+        } 
+    }).catch((error) => {
+        res.status(500).send({ message: error.message }); 
+    });
+});
+
+
+app.delete('/users/:id', async (req, res) => {
+    //console.log(req.params)
+    try {
+        let user = await User.findById(req.params.id)
+        if (!user) {
+            res.status(404).send({ message: 'no user found!' })
+            return
+        }
+        let result = await User.findByIdAndDelete(req.params.id)
+        if (!result) {
+            res.status(404).send({ message: 'cannot delete user!' })
+            return
+        }
+        else {
+            res.send({id: req.params.id})
+            return
+        }
+    }catch(error){
+        res.status(500).send({error: error.message});
+        return
+    }
 });
 
 
